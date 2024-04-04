4233cc281ba4f61e963e1e2b29f13ff311bb8922
Manasee27 <mparulek@usc.edu>
2024-04-03 22:11:25 -0700

    init commit 2

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 require("./routes/apiroutes")(app)
 
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+})
+
 app.listen(PORT, function () {
-console.log('App running on port http://localhost:'  + PORT); 
+    console.log('App running on port http://localhost:'  + PORT); 
 });
